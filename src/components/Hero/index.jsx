9c3c3a61699f9d4df279cd5d85bb350c0750830e
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2023-03-26 01:00:39 -0300

    Update index.jsx

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <div className="hero--container">
             <section className="hero--apresentation">
                 <h1>Magníficos Lanches</h1>
-                <h4>Há mais de 30 anos servindo lanches de qualidade</h4>
+                <h4>Há mais de 30 anos servindo lanches de qualidade.</h4>
                 <button>
                     <a href="https://wa.me/19982192641" rel="external" target="_blank">Peça já</a>
                 </button>
